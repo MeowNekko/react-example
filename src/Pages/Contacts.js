d374068caf28ac1be4f090041ab7897e26cdc90d
Sergey Nilov <s.nilov@mygento.ru>
2022-05-22 20:57:53 +0400

    [contacts] - add form contact us on contacts page

--- src/Pages/Contacts.js
+++ src/Pages/Contacts.js
@@ -1,11 +1,32 @@
 import React, {Component} from 'react';
+import {Button, Container, Form} from "react-bootstrap";
 
 class Contacts extends Component {
     render() {
         return (
-            <div>
-                Hello Contacts
-            </div>
+           <Container style={{width: '500px'}}>
+               <h1 className="text-center"> Contact us </h1>
+               <Form>
+                   <Form.Group controlId="formBasicEmail">
+                       <Form.Label>Email address</Form.Label>
+                       <Form.Control type="email" placeholder="Enter email" />
+                       <Form.Text>
+                           We'll never share your email with anyone else
+                       </Form.Text>
+                   </Form.Group>
+
+                   <Form.Group controlId="formBasicTextarea" className="mt-3">
+                       <Form.Label>Example textarea</Form.Label>
+                       <Form.Control as="textarea" rows="3" />
+                   </Form.Group>
+
+                   <Form.Group controlId="formBasicCheckbox" className="mt-1">
+                       <Form.Check type="checkbox" label="Check my out"/>
+                   </Form.Group>
+
+                   <Button variant="primary" type="submit" className="mt-3">Submit</Button>
+               </Form>
+           </Container>
         );
     }
 }
